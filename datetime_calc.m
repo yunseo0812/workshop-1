beeee0db398f77e19509d61ab265ba109b2ddb13
SEOYUNSEO <yunseo0812@naver.com>
2025-09-08 12:10:49 +0900

    Update datetime_calc.m
    코드 수정

--- datetime_calc.m
+++ datetime_calc.m
@@ -1,17 +1,16 @@
-% datetime_calc.m
-% 날짜/시간 계산 프로그램
-clear
-clc
+clc; clear;
+
 % 사용자 입력
-dateStr = input('현재 날짜와 시간을 입력(예: 2025-00-00 00:00): ', 's');
-hoursToAdd = input('더할 시간을 입력: ');
+dateStr = input('현재 날짜와 시간을 입력 [예: 2025-09-08 10]: ', 's');
 
-% datetime 변환
-currentTime = datetime(dateStr, 'InputFormat', 'yyyy-MM-dd HH:mm');
+% datetime 변환 
+data = datetime(dateStr, 'InputFormat', 'yyyy-MM-dd HH');
 
-% 시간 계산
-newTime = currentTime + hours(hoursToAdd);
+% 더할 시간 입력
+hoursToAdd = input('더할 시간 [시간 단위]: ');
+
+% 계산
+result = data + hours(hoursToAdd);
 
 % 출력
-disp(['결과: ' datestr(newTime, 'yyyy년 mm월 dd일 HH시 MM분')])
-
+fprintf('결과: %s\n', datestr(result, 'yyyy-mm-dd HH'));
