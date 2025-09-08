94d2c9e29e51dd2e3dc6a004d190ec7733c00100
youngwookimm <126767481+youngwookimm@users.noreply.github.com>
2025-09-08 11:51:36 +0900

    Update datetime_calc_youngwoo.m
    - 입력 수정

--- datetime_calc_youngwoo.m
+++ datetime_calc_youngwoo.m
@@ -1,6 +1,3 @@
-% 파일명 예: add_hours_with_datetime.m
-% 문서의 datetime 기능을 활용한 해법
-
 clc; clear;
 
 dtStr = input('Current Datetime [yyyy년 M월 d일 H시]: ','s');
@@ -16,3 +13,4 @@ result = dt + hours(hourAdd);
 
 result.Format = 'yyyy년 M월 d일 H시';
 fprintf('%s\n', char(result));
+
